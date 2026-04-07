74c73ad033a0fbf845a798992c6665ee21e208ef
Orietnom <lucas.leite@ergondata.com.br>
2026-04-07 10:01:02 -0300

    ajuste em timeout de aguardar apareceimento de pop-up

--- src/kmm/services/kmm_actions.py
+++ src/kmm/services/kmm_actions.py
@@ -618,13 +618,13 @@ class KMMActions:
 
                     self.log.info("Formulário preenchido e botão de confirmar clicado com sucesso.")
 
-                    alert = self.driver.wait_alert(180)
+                    alert = self.driver.wait_alert(60)
 
                     if not alert:
                         logger.warning("Pop-Up de confirmação não apareceu após 60 segundos, tentando novamente")
                         self.driver.switch_to_frame(principal=True)
                         self.driver.safe_click('id:btn_confirmar')
-                        alert = self.driver.wait_alert(180)
+                        alert = self.driver.wait_alert(60)
                         if not alert:
                             raise Exception("Pop-up não apareceu")
 
